60e960d8baab8adcf15ab9d0f4d17dcf75046aa0
MrQianGuoPeng <belive0906@163.com>
2022-01-14 12:06:28 +0800

    first commit

---
+++ src/main/java/com/atiguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atiguigu.git;
+
+/**
+ * @desc£º
+ * @author£º QGP
+ * @create£º 2022/1/14 11:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("Hello git !");
+    }
+}
